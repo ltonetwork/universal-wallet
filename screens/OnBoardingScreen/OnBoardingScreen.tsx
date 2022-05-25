85f51ae25765068a17259a57f5f24a4c00c8fa41
ErnestoEsp <ernestoespinoza@wealize.digital>
2022-05-25 16:30:38 +0200

    add styled comp and react paper

--- screens/OnBoardingScreen/OnBoardingScreen.tsx
+++ screens/OnBoardingScreen/OnBoardingScreen.tsx
@@ -43,7 +43,6 @@ export default function OnboardingScreen({ navigation }) {
             }}
             titleStyles={{
 
-
             }}
             subTitleStyles={{
                 justifyContent: 'center'
