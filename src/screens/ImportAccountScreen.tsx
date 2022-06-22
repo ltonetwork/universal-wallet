d1435d95d8f6b3395344f155fc844003814247cb
ErnestoEsp <ernestoespinoza@wealize.digital>
2022-06-22 16:20:00 +0200

    Add pod install to ios folder

--- src/screens/ImportAccountScreen.tsx
+++ src/screens/ImportAccountScreen.tsx
@@ -1,16 +1,27 @@
 import React, { useEffect, useState } from 'react'
-import { SafeAreaView, View } from 'react-native'
+import { SafeAreaView, TouchableOpacity, Text, View, StyleSheet } from 'react-native'
 import { RootStackScreenProps } from '../../types'
 import { StyledTitle, StyledView } from '../components/styles/Signin.styles'
 import { StyledButton } from '../components/styles/StyledButton.styles'
 import { StyledInput } from '../components/styles/StyledInput.styles'
 import LocalStorageService from '../services/LocalStorage.service'
 import { Button } from 'react-native-paper'
+import Clipboard from '@react-native-clipboard/clipboard';
 
 export default function ImportAccountScreen({ navigation, route }: RootStackScreenProps<'Import'>) {
-
+    const [copiedText, setCopiedText] = useState('')
     const [words, setWords] = useState([])
     const [seedPhrase, setSeedPhrase] = useState<string[]>([])
+
+    const copyToClipboard = () => {
+        Clipboard.setString('hello world')
+    }
+
+    const fetchCopiedText = async () => {
+        const text = await Clipboard.getString()
+        setCopiedText(text)
+    }
+
 
 
     useEffect(() => {
@@ -49,6 +60,19 @@ export default function ImportAccountScreen({ navigation, route }: RootStackScre
 
             </StyledInput>
 
+
+            <SafeAreaView style={{ flex: 1 }}>
+                <View style={styles.container}>
+                    <TouchableOpacity onPress={copyToClipboard}>
+                        <Text>Click here to copy to Clipboard</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={fetchCopiedText}>
+                        <Text>View copied text</Text>
+                    </TouchableOpacity>
+
+                    <Text style={styles.copiedText}>{copiedText}</Text>
+                </View>
+            </SafeAreaView>
             {/* {seedPhrase?.map((word) => {
                 <SafeAreaView>
                     <Button>{word}</Button>
@@ -74,5 +98,17 @@ export default function ImportAccountScreen({ navigation, route }: RootStackScre
         </StyledView>
     )
 }
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    copiedText: {
+      marginTop: 10,
+      color: 'red',
+    },
+  });
+  
 
 
