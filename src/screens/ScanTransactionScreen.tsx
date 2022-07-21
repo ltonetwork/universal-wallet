4438211e7c6815415f26b5f7a3256a7fda91a007
ErnestoEsp <ernestoespinoza@wealize.digital>
2022-07-21 18:31:06 +0200

    Verify sender matches, delete unnecesary dependencies

--- src/screens/ScanTransactionScreen.tsx
+++ src/screens/ScanTransactionScreen.tsx
@@ -5,11 +5,14 @@ import { Text } from 'react-native-paper'
 import { RootStackScreenProps } from '../../types'
 import { CenteredView, ScannerContainer, StyledScanner, StyledText } from '../components/styles/ScanScreen.styles'
 import LocalStorageService from '../services/LocalStorage.service'
+import SnackbarMessage from '../components/Snackbar'
 
 
 export default function ScanTransactionScreen({ navigation }: RootStackScreenProps<'ScanTransaction'>) {
     const [isLoading, setIsLoading] = useState<boolean>(true)
     const [permission, setPermission] = useState<boolean>(true)
+    const [snackbarVisible, setSnackbarVisible] = useState(false)
+    const [snackbarMessage, setSnackbarMessage] = useState('')
 
     useEffect(() => {
         requestCameraPermission()
@@ -28,9 +31,12 @@ export default function ScanTransactionScreen({ navigation }: RootStackScreenPro
             })
     }
 
+
     useEffect(() => {
-        if (permission)
+        if (permission) {
             handleConfirmation
+        }
+
     }, [])
 
     const handleConfirmation = async (input: any) => {
@@ -40,17 +46,24 @@ export default function ScanTransactionScreen({ navigation }: RootStackScreenPro
             const lto = new LTO('T')
             const account = lto.account({ seed: myAccount.seed })
             const transfer = JSON.parse(input)
-
             // JUST FOR TESTING PURPOSES
+            transfer.sender = '3MuDkM9nu396vbmEvN6y7uqNPhot7s6ZVaL' // wrong sender
             transfer.timestamp = undefined
             transfer.recipient = "3NAuHWZ7hcyN6Yh7oEuzasTFXa95XMV9baV" // JAVI'S ACCOUNT test purposes
             transfer.amount = 500000000
             //
 
-
-            const transferObject = txFromData(transfer)
-            const signedTransfer = transferObject.signWith(account)
-            await lto.node.broadcast(signedTransfer)
+            if (transfer.sender !== account.address) {
+                setSnackbarMessage('Sender address is not valid')
+                setSnackbarVisible(true)
+            } else {
+                transfer.sender = undefined
+                const transferObject = txFromData(transfer)
+                const signedTransfer = transferObject.signWith(account)
+                await lto.node.broadcast(signedTransfer)
+                setSnackbarMessage('Transfer sent successfully')
+                setSnackbarVisible(true)
+            }
         } catch (error) {
             console.log(error)
         }
@@ -71,7 +84,7 @@ export default function ScanTransactionScreen({ navigation }: RootStackScreenPro
                     onBarCodeScanned={({ data }) => {
                         try {
                             handleConfirmation(data)
-                            navigation.navigate('Root', { screen: 'Wallet' })
+                            navigation.navigate('Root', { screen: 'Wallet' },)
                         }
                         catch (err) {
                             console.log(err)
@@ -79,6 +92,7 @@ export default function ScanTransactionScreen({ navigation }: RootStackScreenPro
                     }}>
                     <StyledText>QR Scanner</StyledText>
                     <StyledText >Scan the QR code from LTO's web application to import your wallet into your mobile phone</StyledText>
+                    {snackbarVisible && <SnackbarMessage text={snackbarMessage} />}
                 </StyledScanner>
             </ScannerContainer>
         )
