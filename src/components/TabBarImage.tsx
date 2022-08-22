cd06e7a55e2f8f551048ba7c692b7349acf806ed
ErnestoEsp <ernestoespinoza@wealize.digital>
2022-08-22 14:52:37 +0200

    Add overview screens icons and test for tabBarImage component

--- src/components/TabBarImage.tsx
+++ src/components/TabBarImage.tsx
@@ -1,22 +1,11 @@
 import React from "react"
-import { Image } from "react-native"
+import { Image, ImageSourcePropType } from "react-native"
 
 
-const credentials = require("../assets/images/credentials-icon.png")
-const inactiveCredentials = require("../assets/images/credentials-icon-inactive.png")
+export default function TabBarImage({ source }: { source: ImageSourcePropType }) {
 
-export default function TabBarImage({ focused }: { focused: boolean }) {
     return (
-        <>
-            {focused
-                ? <Image style={{ marginHorizontal: -7, marginVertical: -3 }} source={credentials} />
-                : <Image style={{ marginHorizontal: -7, marginVertical: -3 }} source={inactiveCredentials} />
-            }
-
-        </>
+        <Image testID="tabIconImage" style={{ marginHorizontal: -7, marginVertical: -4 }} source={source} />
     )
-}
 
-//         <Image source={credentials}></Image>
-//     )
-// }
+}
