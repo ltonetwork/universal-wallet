7d2c033e6d4fbff467ea64052d12a8e3a1e31cea
ErnestoEsp <ernestoespinoza@wealize.digital>
2022-08-23 10:58:48 +0200

    Change background image on credentials, ownables screen. Update changelog

--- src/screens/CredentialsTabScreen/CredentialsTabScreen.tsx
+++ src/screens/CredentialsTabScreen/CredentialsTabScreen.tsx
@@ -1,25 +1,29 @@
 import React from 'react'
+import { ImageBackground, useWindowDimensions } from 'react-native'
 import { RootTabScreenProps } from '../../../types'
 import OverviewHeader from '../../components/OverviewHeader'
 import QRButton from '../../components/QRButton'
 import SocialMediaIcon from '../../components/SocialMediaIcon'
 import StatusBarIOS from '../../components/StatusBarIOS'
-import { Container, IconContainer, MainTitle, StyledImage, StyledText, StyledTitle, StyledView } from '../../components/styles/NextFunctionality.styles'
-import { placeholderImage, socialMediaIcons } from '../../utils/images'
+import { Container, IconContainer, MainTitle, StyledText, StyledTitle, StyledView } from '../../components/styles/NextFunctionality.styles'
+import { backgroundImage, socialMediaIcons } from '../../utils/images'
 import { navigateToFacebook, navigateToLinkedin, navigateToTelegram, navigateToTwitter } from '../../utils/redirectSocialMedia'
 
+
 export default function CredentialsTabScreen({ navigation }: RootTabScreenProps<'Credentials'>) {
+
+  const { width, height } = useWindowDimensions()
 
   return (
     <>
       <StatusBarIOS backgroundColor={'#ffffff'} />
+      <ImageBackground source={backgroundImage} style={{ width, height, position: "absolute" }} />
       <Container>
         <OverviewHeader
           icon={"menu"}
           onPress={() => navigation.navigate('Modal')}
           input={<MainTitle>Credentials</MainTitle>} />
         <StyledView>
-          <StyledImage source={placeholderImage}></StyledImage>
           <StyledTitle>This feature will soon be available</StyledTitle>
           <StyledText>Follow us on our social networks to stay up to date with the latest news about the application.</StyledText>
           <IconContainer>
