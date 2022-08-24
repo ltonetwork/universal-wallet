de0cf3c7754b0df99deb4251ee3cf1a0185f0bf2
ErnestoEsp <ernestoespinoza@wealize.digital>
2022-08-24 15:47:29 +0200

    Change profile screen title's color to black and remove back title on ios

--- src/screens/OnBoardingScreen/OnBoardingScreen.tsx
+++ src/screens/OnBoardingScreen/OnBoardingScreen.tsx
@@ -1,10 +1,10 @@
 import React, { useRef, useState } from 'react'
-import { FlatList, StatusBar, useWindowDimensions } from 'react-native'
+import { FlatList, useWindowDimensions } from 'react-native'
 import Footer from '../../components/Footer'
 import Header from '../../components/Header'
 import Slide from '../../components/Slide'
-import { StyledSafeAreaView } from './OnBoardingScreen.styles'
 import slides from '../../utils/slideList'
+import { StyledSafeAreaView } from './OnBoardingScreen.styles'
 
 export default function OnboardingScreen() {
 
