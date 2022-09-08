017ffa4a8f23726e574ea8dc3ff34d4d42a5a359
ErnestoEsp <ernestoespinoza@wealize.digital>
2022-09-08 14:31:20 +0200

    Changes applied

--- src/screens/ImportAccountScreen/ImportAccountScreen.tsx
+++ src/screens/ImportAccountScreen/ImportAccountScreen.tsx
@@ -24,6 +24,7 @@ export default function ImportAccountScreen({ navigation }: RootStackScreenProps
     const [checked, setChecked] = useState<boolean>(false)
     const [modalVisible, setModalVisible] = useState<boolean>(false)
     const [accountAddress, setAccountAddress] = useState('')
+    const { setShowMessage, setMessageInfo } = useContext(MessageContext)
 
     useEffect(() => {
         getAccountAddress()
@@ -32,9 +33,15 @@ export default function ImportAccountScreen({ navigation }: RootStackScreenProps
     const getAccountAddress = () => {
         LocalStorageService.getData('@accountData')
             .then(data => {
-                const account = data
-                setIsLoading(false)
-                setAccountAddress(account[0].address)
+                if (data === null) {
+                    setShowMessage(true)
+                    setMessageInfo('Error creating/importing your account!')
+                    navigation.goBack()
+                } else {
+                    const account = data
+                    setIsLoading(false)
+                    setAccountAddress(account[0]?.address)
+                }
             })
             .catch(err => console.log(err))
     }
@@ -42,8 +49,6 @@ export default function ImportAccountScreen({ navigation }: RootStackScreenProps
     const handleInputChange = (name: string, value: string) => {
         setloginForm({ ...loginForm, [name]: value })
     }
-
-    const { setShowMessage, setMessageInfo } = useContext(MessageContext)
 
     const handleImportAccount = () => {
         if (loginForm.nickname === '') {
