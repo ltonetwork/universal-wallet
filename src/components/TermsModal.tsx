ae7a62a561f15ed58de6489274069539f30ac093
ErnestoEsp <ernestoespinoza@wealize.digital>
2022-09-22 17:52:56 +0200

    Remove vertical scroll indicator terms modal

--- src/components/TermsModal.tsx
+++ src/components/TermsModal.tsx
@@ -1,7 +1,7 @@
 import React from "react"
-import { Modal, ScrollView } from "react-native"
+import { Modal } from "react-native"
 import { REGISTER, TERMS_AND_CONDITIONS_CONTENT } from "../constants/Text"
-import { Container, ModalText, ModalView } from '././styles/TermsModal.styles'
+import { Container, ModalText, ModalView, TextContainer } from '././styles/TermsModal.styles'
 import { StyledButton } from "./styles/StyledButton.styles"
 
 
@@ -14,15 +14,16 @@ export default function TermsModal(props: {
     return (
 
         <Modal
-            animationType="slide"
-            transparent={true}
+            animationType='slide'
+            transparent={false}
             visible={props.visible}
             onRequestClose={() => props.onRequestClose()}
         >
             <Container>
                 <ModalView>
                     <ModalText>{REGISTER.MODAL_TITLE}</ModalText>
-                    <ScrollView>
+                    <TextContainer showsVerticalScrollIndicator={false}
+                    >
                         <ModalText>{TERMS_AND_CONDITIONS_CONTENT}</ModalText>
                         <Container>
                             <StyledButton
@@ -36,7 +37,7 @@ export default function TermsModal(props: {
                                 {REGISTER.BUTTON_MODAL}
                             </StyledButton>
                         </Container>
-                    </ScrollView>
+                    </TextContainer>
                 </ModalView>
             </Container>
         </Modal>
