aa65f1b88219d1e3a207c10491d7c823cfe0658c
ErnestoEsp <ernestoespinoza@wealize.digital>
2022-09-22 17:54:51 +0200

    Draw checkbox square on ios

---
+++ src/components/styles/CheckBox.styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components/native"
+
+export const CheckBoxContainer = styled.View`
+    flex-direction: row; 
+    padding: 20px;
+`
+
+export const CheckBoxLabel = styled.Text`
+    color: #A017B7;
+    marginTop: 10px; 
+    marginLeft: 20px;
+`
