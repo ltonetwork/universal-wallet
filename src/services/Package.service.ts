f38b02e93fc1cdf672de0fb7fbb5a59eadeb49b6
bekauz <bekauz@protonmail.com>
2022-11-16 11:50:39 +0100

    ownable action button

---
+++ src/services/Package.service.ts
@@ -0,0 +1,11 @@
+import { LocalStorageService } from "./LocalStorageService";
+import { zip, unzip, unzipAssets, subscribe } from 'react-native-zip-archive';
+
+
+export default class PackageService {
+
+
+    public static unzipOwnable = async (documentPickerResponse: DocumentPickerResponse) => {
+//         unzip()
+    }
+}
