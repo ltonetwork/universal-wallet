0ad6d5a985bce637c4c7fc31a82c80f0b8675579
bekauz <bekauz@protonmail.com>
2022-11-18 14:01:54 +0100

    storing ownable package contents in localStorage

--- src/services/Package.service.ts
+++ src/services/Package.service.ts
@@ -1,30 +1,29 @@
-import { LocalStorageService } from "./LocalStorageService";
+import LocalStorageService from './LocalStorage.service'
 import { zip, unzip, unzipAssets, subscribe } from 'react-native-zip-archive';
-
+import RNFS from 'react-native-fs';
 
 export default class PackageService {
 
-
     public static unzipOwnable = async (documentPickerResponse: DocumentPickerResponse) => {
-        console.log("unzipping ownable");
-        console.log(documentPickerResponse);
         const name = documentPickerResponse.name;
         const nameNoExtension = name.substring(0, name.indexOf('.zip'));
-        const type = documentPickerResponse.type;
-        const uri = documentPickerResponse.uri;
         const fileCopyUri = documentPickerResponse.fileCopyUri;
         const ownableTargetUri = `${fileCopyUri.substring(0, fileCopyUri.indexOf(name))}${nameNoExtension}/`;
 
-        console.log("PATH TARGET: ", ownableTargetUri);
-        console.log("PATH SOURCE: ", fileCopyUri);
-
-       console.log();
-
-        unzip(fileCopyUri, ownableTargetUri)
-        .then((path) => {
+        unzip(`${fileCopyUri}/`, ownableTargetUri)
+        .then(async (path) => {
           console.log(`unzip completed at ${path}`)
+          const directoryFiles = await RNFS.readDir(ownableTargetUri);
+          for (let i = 0; i < directoryFiles.length; i++) {
+            const file = directoryFiles[i];
+            console.log("\n Storing file in local storage: ");
+            console.log(file);
+            await LocalStorageService.storeData(file.name, file);
+          }
+          await LocalStorageService.storeData(`${nameNoExtension}-uri-path`, fileCopyUri);
         })
         .catch((error) => {
+          console.error(`\n\n unzip failed at ${fileCopyUri} \n\n`)
           console.error(error)
         })
     }
