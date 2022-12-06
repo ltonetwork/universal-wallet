edd86fc0b49bba6d422a41575538813875a9a2be
bekauz <bekauz@protonmail.com>
2022-12-06 15:51:43 +0100

    WIP: worker attempt with blobs

--- src/services/WASM.service.ts
+++ src/services/WASM.service.ts
@@ -1,6 +1,7 @@
 import LocalStorageService from './LocalStorage.service'
 import { Thread } from 'react-native-threads'
 import RNFS from 'react-native-fs'
+import * as FileSystem from 'expo-file-system';
 
 export default class WASMService {
 
@@ -25,18 +26,40 @@ export default class WASMService {
     thread.onerror = (err) => console.error(err);
     thread.onmessageerror = (err) => console.error(err);
 
-    RNFS.readDir(RNFS.DocumentDirectoryPath)
+    RNFS.readDir(RNFS.DocumentDirectoryPath + `/${ownableType}`)
       .then((result) => {
-        return Promise.all([RNFS.stat(result[0].path), result[0].path]);
+        console.log(result);
+        return Promise.all([RNFS.stat(result[5].path), result[5].path]);
       })
       .then((statResult) => {
+        console.log("stat result: ", statResult);
         if (statResult[0].isFile()) {
-          return RNFS.readFile(statResult[1], 'utf8');
+          const file = RNFS.readFile(statResult[1], 'base64');
+          return file;
         }
         return 'no file';
       })
-      .then((contents) => {
-        console.log(contents);
+      .then(async (contents) => {
+        console.log("File contents", contents);
+//            let blob = await fetch(`file://${contents.path}`).blob()
+
+//         let fr = new FileReader();
+//         fr.onload = (f) => {
+//           console.log("fr read file ", f);
+//         }
+//         fr.readAsText(contents);
+//         let wasm = await fetch("./ownable_potion_bg.wasm");
+//         const prefix = "data:text/javascript;base64,";
+//         const fileSrc = prefix + contents;
+//
+//         const byteNumbers = new Array(fileSrc.length);
+//         for (let i = 0; i < fileSrc.length; i++) {
+//             byteNumbers[i] = fileSrc.charCodeAt(i);
+//         }
+//         const byteArray = new Uint8Array(byteNumbers);
+//         const blob = new Blob([byteArray], {type: 'application/javascript'});
+//
+//         console.log(fileSrc);
       })
       .catch((err) => {
         console.log(err.message, err.code);
