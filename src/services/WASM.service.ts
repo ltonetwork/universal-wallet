7f18466da2a1bbe915fb11938f7253f263145258
bekauz <bekauz@protonmail.com>
2022-12-07 16:38:30 +0100

    WIP: text blob init attempt

--- src/services/WASM.service.ts
+++ src/services/WASM.service.ts
@@ -18,31 +18,40 @@ export default class WASMService {
         console.log("stat result: ", statResult);
         if (statResult[0].isFile()) {
           console.log('reading file ', statResult[1]);
-          const file = RNFS.readFile(statResult[1], 'base64');
+          const file = RNFS.readFile(statResult[1], 'utf8');
           return file;
         }
         return 'no file';
       })
       .then(async (contents) => {
         console.log("File contents", contents);
-        const byteCharacters = atob(contents);
-        const byteArrays = [];
-        const sliceSize = 512;
-        const contentType = '';
-
-        for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
-          const slice = byteCharacters.slice(offset, offset + sliceSize);
-
-          const byteNumbers = new Array(slice.length);
-          for (let i = 0; i < slice.length; i++) {
-            byteNumbers[i] = slice.charCodeAt(i);
-          }
-          const byteArray = new Uint8Array(byteNumbers);
-          byteArrays.push(byteArray);
-        }
-
-        const blob = new Blob(byteArrays, { type: contentType });
-        console.log("blob", blob);
+        const blob = new Blob([`(${contents})()`]);
+        let worker = new Worker(URL.createObjectURL(blob));
+//         const byteCharacters = atob(contents);
+//         const byteArrays = [];
+//         const sliceSize = 512;
+//
+//         for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+//           const slice = byteCharacters.slice(offset, offset + sliceSize);
+//
+//           const byteNumbers = new Array(slice.length);
+//           for (let i = 0; i < slice.length; i++) {
+//             byteNumbers[i] = slice.charCodeAt(i);
+//           }
+//           const byteArray = new Uint8Array(byteNumbers);
+//           byteArrays.push(byteArray);
+//         }
+//         const blob = new Blob([byteArrays.buffer], { type: `application/javascript` });
+//
+//         console.log("blob", blob);
+//
+//         const reader = new FileReader();
+//         reader.addEventListener("loadend", () => {
+//           console.log(reader.result);
+//         });
+//         reader.readAsArrayBuffer(blob);
+//         const blobURL = URL.createObjectURL(blob);
+//         console.log("blobURL", blobURL);
       })
       .catch((err) => {
         console.log(err.message, err.code);
