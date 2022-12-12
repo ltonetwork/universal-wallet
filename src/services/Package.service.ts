bfe2696dd0ed019ef2fb04322bf1684847c99f65
arnold <arnold@jasny.net>
2022-12-12 14:42:31 -0400

    Clean up ownables tab. Move logic of loading ownable options to package service.

--- src/services/Package.service.ts
+++ src/services/Package.service.ts
@@ -1,62 +1,54 @@
 import LocalStorageService from './LocalStorage.service'
-import { zip, unzip, unzipAssets, subscribe } from 'react-native-zip-archive';
-import RNFS from 'react-native-fs';
-import RNFetchBlob from "rn-fetch-blob";
-import { Asset } from 'expo-asset';
+import { unzip } from 'react-native-zip-archive'
+import RNFS from 'react-native-fs'
+import RNFetchBlob from "rn-fetch-blob"
+import {DocumentPickerResponse} from "react-native-document-picker"
 
 export default class PackageService {
 
-    public static unzipOwnable = async (documentPickerResponse: DocumentPickerResponse) => {
-        const name = documentPickerResponse.name;
-        const nameNoExtension = name.substring(0, name.indexOf('.zip'));
-        const fileCopyUri = documentPickerResponse.fileCopyUri;
-        const ownableTargetUri = `${fileCopyUri.substring(0, fileCopyUri.indexOf(name))}${nameNoExtension}/`;
+    public static getOwnableOptions = async() => LocalStorageService.getData('ownable-options')
 
-        const targetPath = RNFS.DocumentDirectoryPath + `/${nameNoExtension}`;
+    public static importOwnable = async (documentPickerResponse: DocumentPickerResponse) => {
+        const name = documentPickerResponse.name!
+        const nameNoExtension = name.substring(0, name.indexOf('.zip'))
+        const fileCopyUri = documentPickerResponse.fileCopyUri!
+        const ownableTargetUri = `${fileCopyUri.substring(0, fileCopyUri.indexOf(name))}${nameNoExtension}/`
 
-        // path where assets, resolves to bundle-assets://threads/test.txt
-        let myAsset= RNFetchBlob.fs.asset('threads/test.txt');
+        const targetPath = RNFS.DocumentDirectoryPath + `/${nameNoExtension}`
 
-        unzip(`${fileCopyUri}/`, targetPath)
-        .then(async (path) => {
-          const directoryFiles = await RNFS.readDir(targetPath);
-          for (let i = 0; i < directoryFiles.length; i++) {
-            const file = directoryFiles[i];
-            await LocalStorageService.storeData(file.name, file);
+        await unzip(`${fileCopyUri}/`, targetPath)
+
+        const directoryFiles = await RNFS.readDir(targetPath)
+        for (const file of directoryFiles) {
+            await LocalStorageService.storeData(file.name, file)
             if (file.name.endsWith(".js")) {
-              RNFetchBlob.fs.stat(file.path)
-                  .then((stats) => console.log(stats))
-                  .catch((err) => {})
+                RNFetchBlob.fs.stat(file.path)
+                    .then((stats) => console.log(stats))
+                    .catch((err) => {})
 
-              await LocalStorageService.storeData(`${nameNoExtension}-bindgen`, file.path);
+                await LocalStorageService.storeData(`${nameNoExtension}-bindgen`, file.path)
             }
-          }
-          console.log(`unzip completed at ${targetPath}`);
-        })
-        .then(async () => {
-          await PackageService.addOwnableOption(nameNoExtension);
-        })
-        .catch((error) => {
-          console.error(`\n\n unzip failed at ${targetPath} \n\n`)
-          console.error(error)
-        });
+        }
+        console.log(`unzip completed at ${targetPath}`)
+
+        try {
+            await PackageService.addOwnableOption(nameNoExtension)
+        } catch (error) {
+            console.error(`\n\n unzip failed at ${targetPath} \n\n`)
+            console.error(error)
+        }
     }
 
-
     public static addOwnableOption = async (ownableName: string) => {
-      var ownableOptions = await LocalStorageService.getData('ownable-options');
-      console.log("existing ownable options: ", ownableOptions);
-      if (!ownableOptions) {
-        ownableOptions = [];
-      }
+        const ownableOptions: {name: string}[] = (await LocalStorageService.getData('ownable-options')) || []
 
-      if (!ownableOptions.find(option => option.name === ownableName)) {
-        const ownableOption = { 'name': ownableName, 'id': ownableOptions.length };
-        console.log("ownable option: ", ownableOption);
-        ownableOptions.push(ownableOption);
-        await LocalStorageService.storeData('ownable-options', ownableOptions);
-      } else {
-        console.log("Ownable option already exists");
-      }
+        if (!ownableOptions.find(option => option.name === ownableName)) {
+            const ownableOption = { 'name': ownableName, 'id': ownableOptions.length }
+            console.log("ownable option: ", ownableOption)
+            ownableOptions.push(ownableOption)
+            await LocalStorageService.storeData('ownable-options', ownableOptions)
+        } else {
+            console.log("Ownable option already exists")
+        }
     }
 }
--- src/services/WASM.service.ts
+++ src/services/WASM.service.ts
@@ -1,68 +1,70 @@
-import LocalStorageService from './LocalStorage.service'
-import { Thread } from 'react-native-threads'
 import RNFS from 'react-native-fs'
-import * as FileSystem from 'expo-file-system';
-import {decode as atob, encode as btoa} from 'base-64'
+//import { Thread } from 'react-native-threads'
 
 export default class WASMService {
+    public static spawnWASMThread = async (ownableType: string) => {
+        console.log(`spawning ${ownableType} thread...`)
 
-  public static spawnWASMThread = async (ownableType: string) => {
-    console.log(`spawning ${ownableType} thread...`);
+        const result = await RNFS.readDir(RNFS.DocumentDirectoryPath + `/${ownableType}`)
+        console.log(result)
 
-    RNFS.readDir(RNFS.DocumentDirectoryPath + `/${ownableType}`)
-      .then((result) => {
-        console.log(result);
-        return Promise.all([RNFS.stat(result[5].path), result[5].path]);
-      })
-      .then((statResult) => {
-        console.log("stat result: ", statResult);
-        if (statResult[0].isFile()) {
-          console.log('reading file ', statResult[1]);
-          const file = RNFS.readFile(statResult[1], 'utf8');
-          return file;
+        const jsFile = result.find(file => file.name === 'ownable.js' || file.name === `ownable_${ownableType}.js`)
+        if (!jsFile) throw new Error('Package doesn\'t contain an ownable.js file')
+
+        const statResult = await RNFS.stat(jsFile.path)
+        console.log("stat result: ", statResult)
+
+        if (!statResult.isFile()) {
+            throw new Error('no file')
         }
-        return 'no file';
-      })
-      .then(async (contents) => {
-        console.log("File contents", contents);
-        const blob = new Blob([`(${contents})()`]);
-        let worker = new Worker(URL.createObjectURL(blob));
-//         const byteCharacters = atob(contents);
-//         const byteArrays = [];
-//         const sliceSize = 512;
+
+        console.log('reading file ', jsFile.path)
+        const contents = RNFS.readFile(jsFile.path, 'utf8')
+
+        /*const thread = new Thread('worker.js')
+        thread.postMessage(contents)*/
+    }
+}
+
+// === Try using a worker. Code can probably be removed. ===
 //
-//         for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
-//           const slice = byteCharacters.slice(offset, offset + sliceSize);
+//        let worker = new Worker(URL.createObjectURL(blob))
+//         const byteCharacters = atob(contents)
+//         const byteArrays = []
+//         const sliceSize = 512
 //
-//           const byteNumbers = new Array(slice.length);
-//           for (let i = 0; i < slice.length; i++) {
-//             byteNumbers[i] = slice.charCodeAt(i);
+//         for (let offset = 0 offset < byteCharacters.length offset += sliceSize) {
+//           const slice = byteCharacters.slice(offset, offset + sliceSize)
+//
+//           const byteNumbers = new Array(slice.length)
+//           for (let i = 0 i < slice.length i++) {
+//             byteNumbers[i] = slice.charCodeAt(i)
 //           }
-//           const byteArray = new Uint8Array(byteNumbers);
-//           byteArrays.push(byteArray);
+//           const byteArray = new Uint8Array(byteNumbers)
+//           byteArrays.push(byteArray)
 //         }
-//         const blob = new Blob([byteArrays.buffer], { type: `application/javascript` });
+//         const blob = new Blob([byteArrays.buffer], { type: `application/javascript` })
 //
-//         console.log("blob", blob);
+//         console.log("blob", blob)
 //
-//         const reader = new FileReader();
+//         const reader = new FileReader()
 //         reader.addEventListener("loadend", () => {
-//           console.log(reader.result);
-//         });
-//         reader.readAsArrayBuffer(blob);
-//         const blobURL = URL.createObjectURL(blob);
-//         console.log("blobURL", blobURL);
-      })
-      .catch((err) => {
-        console.log(err.message, err.code);
-      });
+//           console.log(reader.result)
+//         })
+//         reader.readAsArrayBuffer(blob)
+//         const blobURL = URL.createObjectURL(blob)
+//         console.log("blobURL", blobURL)
+//        })
+//        .catch((err) => {
+//          console.log(err.message, err.code)
+//        })
 
 //     RNFS.readFileAssets("ownable_potion_bg.wasm", "base64")
 //     .then(binary => {
-//       console.log(binary);
+//       console.log(binary)
 //     })
 //     .catch(console.error)
-//     const wasmArrayBuffer = await getBlobFromObjectStoreAsArrayBuffer(ownableType, "wasm");
-//     worker.postMessage(wasmArrayBuffer, [wasmArrayBuffer]);
-  }
-}
+//     const wasmArrayBuffer = await getBlobFromObjectStoreAsArrayBuffer(ownableType, "wasm")
+//     worker.postMessage(wasmArrayBuffer, [wasmArrayBuffer])
+//  }
+//}
