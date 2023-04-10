67d6bcf59713d227a6c3e7fce26a0b6de1230023
ErnestoEsp <emenocal07@gmail.com>
2023-04-10 13:16:33 +0200

    Modify authenticateWithBiometrics Fn

--- src/utils/authenticateWithBiometrics.ts
+++ src/utils/authenticateWithBiometrics.ts
@@ -1,8 +1,8 @@
 import { NativeStackNavigationProp } from '@react-navigation/native-stack'
-import LTOService from '../services/LTO.service'
-import { getPassword } from './keychain'
 import { RootStackParamList } from '../../types'
-import * as LocalAuthentication from 'expo-local-authentication'
+import ReactNativeBiometrics from 'react-native-biometrics'
+import LTOService from '../services/LTO.service'
+
 
 interface Props {
   navigation: NativeStackNavigationProp<RootStackParamList, 'LockedScreen' | 'SignIn'>
@@ -10,46 +10,35 @@ interface Props {
 }
 
 export const authenticateWithBiometrics = async ({ navigation, goBack }: Props): Promise<void> => {
+
+  const rnBiometrics = new ReactNativeBiometrics()
+
+  const isEnrolled = (await rnBiometrics.biometricKeysExist()).keysExist
+
+  if (!isEnrolled) {
+    return
+  }
+
   try {
-    const hasHardwareSupport: boolean = await LocalAuthentication.hasHardwareAsync()
-    const isEnrolledOnDevice: boolean = await LocalAuthentication.isEnrolledAsync()
+    const resultObject = await rnBiometrics.createSignature({
+      promptMessage: 'Authenticate',
+      payload: 'payload'
+    })
 
-    if (!hasHardwareSupport) {
-      throw new Error('Biometric authentication is not available on this device')
-    }
+    const { success, signature } = resultObject
 
-    const passCode: string | null = await getPassword()
+    if (!success) return
 
-    if (!passCode) {
-      throw new Error('Unable to retrieve password.')
-    }
-
-    if (isEnrolledOnDevice) {
-      const result = await LocalAuthentication.authenticateAsync({
-        promptMessage: 'Authenticate',
-        cancelLabel: 'Cancel',
-      })
-
-      if (!result.success) {
-        throw new Error('Authentication failed.')
-      }
-
-      await LTOService.unlock(passCode)
+    if (success && signature) {
+      await LTOService.unlock(undefined, signature)
       setTimeout(() => {
-        goBack && navigation.goBack() || navigation.navigate('Root')
+        goBack ? navigation.goBack() : navigation.navigate('Root')
       }, 1000)
+
     } else {
-      await LocalAuthentication.authenticateAsync({
-        promptMessage: 'You must be enrolled on your device to use biometrics',
-        cancelLabel: 'Cancel',
-      })
-
-      await LTOService.unlock(passCode)
-      setTimeout(() => {
-        goBack && navigation.goBack() || navigation.navigate('Root')
-      }, 1000)
+      throw new Error('Authentication failed')
     }
-  } catch (error: unknown) {
-    console.error(`Error during biometric authentication: ${error}`)
+  } catch (error) {
+    throw new Error(`Error during biometric authentication: ${error}`)
   }
 }
