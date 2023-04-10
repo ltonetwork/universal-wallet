7fa420fcfcf2ca37fb106b9bffd115e955c8cfe8
ErnestoEsp <emenocal07@gmail.com>
2023-04-10 18:19:58 +0200

    Remove react-native-keychain library

--- android/app/src/main/java/lto/net/com/MainApplication.java
+++ android/app/src/main/java/lto/net/com/MainApplication.java
@@ -4,10 +4,8 @@ import android.app.Application;
 import android.content.Context;
 import android.content.res.Configuration;
 import androidx.annotation.NonNull;
-
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.oblador.keychain.KeychainPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
