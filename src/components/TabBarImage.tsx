d619bfc399651d9e11e1e2d96fbfd82461980769
ErnestoEsp <emenocal07@gmail.com>
2023-05-18 17:28:53 +0200

    Resize tab images (check if this is ok)

--- src/components/TabBarImage.tsx
+++ src/components/TabBarImage.tsx
@@ -5,7 +5,7 @@ import { Image, ImageSourcePropType } from "react-native"
 export default function TabBarImage({ source }: { source: ImageSourcePropType }): JSX.Element {
 
     return (
-        <Image testID="tabIconImage" style={{ marginHorizontal: -7, marginVertical: -4 }} source={source} />
+        <Image testID="tabIconImage" style={{ marginHorizontal: -7, marginVertical: -4, width: 35, height: 35 }} source={source} />
     )
 
 }
