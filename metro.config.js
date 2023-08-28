0cf02e24635220e934a454cf4fd769b5ed7527d6
Ernesto Espinoza Menocal <emenocal07@gmail.com>
2023-08-28 13:44:41 +0200

    Add comment to remove warning

--- metro.config.js
+++ metro.config.js
@@ -1,3 +1,16 @@
 const { getDefaultConfig } = require('expo/metro-config')
 
 module.exports = getDefaultConfig(__dirname)
+
+
+// const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+
+// /**
+//  * Metro configuration to remove warning
+//  * https://facebook.github.io/metro/docs/configuration
+//  *
+//  * @type {import('metro-config').MetroConfig}
+//  */
+// const config = {};
+
+// module.exports = mergeConfig(getDefaultConfig(__dirname), config);
